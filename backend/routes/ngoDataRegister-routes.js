cd125c4892e30694ff45d826b726db5b9e2141d2
Vikram Chiluka <vikramchiluka18@gmail.com>
2022-06-05 09:37:22 +0530

    final commit

--- backend/routes/ngoDataRegister-routes.js
+++ backend/routes/ngoDataRegister-routes.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ngoDataRegistrationController = require("../controllers/ngoDataRegistrationController");
 
-
-
-
-
 router.post("/addNgoData", ngoDataRegistrationController.addNgoRegistration);
 router.get("/getAllNgos", ngoDataRegistrationController.getAllNgosData);
 router.get("/getNgoData/:id", ngoDataRegistrationController.getNgoData);
